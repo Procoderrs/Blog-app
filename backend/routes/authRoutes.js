5d4b036c82e6cf6aa3061aa9fa1f880e90e5a18e
Zeenat <zeenat.zeni0112@gmail.com>
2025-11-28 10:59:52 +0500

    commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { registerUser,loginUser } from '../controllers/authControllers.js';
+import protect from '../middleware/authMidleware.js';
 
 const router=express.Router();
 
-router.post('/register',registerUser);
-router.post('/login',loginUser);
+router.post('/register',protect,registerUser);
+router.post('/login',protect,loginUser);
 
 export default router;
