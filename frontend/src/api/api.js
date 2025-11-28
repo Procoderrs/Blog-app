587b6a6a6365991c1cfd55ee7ed1eca28974a34e
Zeenat <zeenat.zeni0112@gmail.com>
2025-11-28 12:40:36 +0500

    commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api=axios.create({
-   baseURL: import.meta.env.VITE_API_URI,
+   baseURL: import.meta.env.VITE_API_URI/api,
     
 /*    baseURL:'http://localhost:5000/api', 
  */ 
