01ad3396d6e4ce1338e9d271e52cec3367206054
Zeenat <zeenat.zeni0112@gmail.com>
2025-12-04 12:33:16 +0500

    commit

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,14 +1,17 @@
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../utils/cloudinary.js";
 
+// For v2.x use DEFAULT IMPORT
+import CloudinaryStorage from "multer-storage-cloudinary";
+
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary,
   params: {
-    folder: "blog_images",           // optional: folder in Cloudinary
+    folder: "blog_images",
     allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
 
 const upload = multer({ storage });
+
 export default upload;
--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -9,9 +9,5 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
-console.log("Cloudinary Config:", {
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY
-});
 
 export default cloudinary;
