fceb23c7b71ef753f13aa0f08e4f5cb4feb2f14e
Zeenat <zeenat.zeni0112@gmail.com>
2025-12-08 14:47:23 +0500

    commit

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -7,12 +7,12 @@ import { deletePost } from '../controllers/postController.js';
 
 const router = express.Router();
 
-/* router.post('/category', protect, createCategory);
+router.post('/category', protect, createCategory);
 router.get('/categories' , getCategories);
 
 // adminRoutes.js
 router.put("/category/:id", protect,  updateCategory);
-router.delete("/category/:id", protect, deleteCategory); */
+router.delete("/category/:id", protect, deleteCategory);
 
 router.get('/users',protect,adminOnly,getAllUsers);
 router.get('/users/:id/posts',protect,adminOnly,getUserPosts)
